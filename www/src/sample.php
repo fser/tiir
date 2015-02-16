dfc52eb6dcfc1f33863ca38a9ab9e5d3a470339b
Francois <fser@code-libre.org>
2015-02-16 15:41:55 +0100

    modify sample page

--- www/src/sample.php
+++ www/src/sample.php
@@ -1,6 +1,12 @@
 <?php
 include('db.php');
+$tail = '';
+if (isset($_GET['id']))
+{
+        $tail = 'AND id = ' . $_GET['id'];
+}
 
-$res = mysql_query('SELECT * FROM news WHERE id = "' . $_GET['id'] . '"');
+$res = mysql_query('SELECT * FROM news WHERE 1 ' . $tail);
+echo '<pre>';
 while ($row = mysql_fetch_assoc($res))
   print_r($row);
